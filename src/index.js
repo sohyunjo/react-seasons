1b27547b66ab86d5389eb72d4af015d832d3833a
So Hyun Jo <sohyunjo@Sos-MacBook-Pro.home>
2019-03-28 22:33:17 -0400

    added spinner and styling

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import SeasonDisplay from './SeasonDisplay'
+import Spinner from './Spinner';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends React.Component{
+    // constructor(props) {
+    //     super(props);
+
+    //     this.state = {lat: null, errorMessage: ''};
+    // }
+
+    state = {lat: null, errorMessage: ''};
+
+    componentDidMount(){
+
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({lat: position.coords.latitude})
+            },
+            (err) => {
+                this.setState({errorMessage: err.message})
+            }
+        );
+    }
+
+    render() {
+        if(this.state.errorMessage && !this.state.lat){
+            return <div> Error: {this.state.errorMessage}</div>;
+        }
+        else if(!this.state.errorMessage && this.state.lat){
+            // return <div>Latitude: {this.state.lat}</div>;
+            return <SeasonDisplay lat={this.state.lat} />;
+        }
+        
+        return <Spinner />;
+        
+    }
+}
+
+ReactDOM.render( <App />, document.querySelector('#root'))
