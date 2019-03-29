752a588d76964f1eb0e4cdc3bd1e6b99eb6b8ee4
So Hyun Jo <sohyun@vt.edu>
2019-03-28 23:32:19 -0400

    done

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ class App extends React.Component{
         );
     }
 
-    render() {
+    renderContent(){
         if(this.state.errorMessage && !this.state.lat){
             return <div> Error: {this.state.errorMessage}</div>;
         }
@@ -34,8 +34,16 @@ class App extends React.Component{
             return <SeasonDisplay lat={this.state.lat} />;
         }
         
-        return <Spinner />;
+        return <Spinner message="Please accept location request"/>;
         
+    }
+
+    render() {
+        return(
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        );
     }
 }
 
